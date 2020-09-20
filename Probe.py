cf8102ac2c52c2299d6e4c5610a2a44c2e826ae1
Serg Gurzhy <serg6340706@gmail.com>
2020-09-20 22:28:56 +0300

    Change2

--- Probe.py
+++ Probe.py
@@ -1,7 +1,7 @@
 # Цикл for
 start=input("Введите начальное значение больше 0   ")
-stop=input("Введите конечное значение   ")
-step=input("Введите шаг операции   ")
+stop=input("Введите конечное значение до 100  ")
+step=input("Введите шаг операции. Только целое число  ")
 start=int(start)
 stop=int(stop)
 step=int(step)
