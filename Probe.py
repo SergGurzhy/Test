69a48bb27b9f39e46ce893b7a48644be0d4130bc
Serg Gurzhy <serg6340706@gmail.com>
2020-09-20 21:21:40 +0300

    Add Probe.py

---
+++ Probe.py
@@ -0,0 +1,17 @@
+# Цикл for
+start=input("Введите начальное значение   ")
+stop=input("Введите конечное значение   ")
+step=input("Введите шаг операции   ")
+start=int(start)
+stop=int(stop)
+step=int(step)
+
+for x in range(start,stop,step):
+    print(x)
+     
+    
+    
+    
+
+
+            
